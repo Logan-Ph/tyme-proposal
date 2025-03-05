7c2268fb94c701df5aa5f01abd6ea929a48e08d3
Logan-Ph <118104996+Logan-Ph@users.noreply.github.com>
2025-03-05 14:50:06 +0700

    init

---
+++ src/main/java/com/tour/booking/tyme/TymeApplication.java
@@ -0,0 +1,13 @@
+package com.tour.booking.tyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tour/booking/tyme/TymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tour.booking.tyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
